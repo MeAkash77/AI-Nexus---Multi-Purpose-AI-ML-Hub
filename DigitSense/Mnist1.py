f4a6386dbfc54d2d747223d2fff79fc2aab6276d
Me Akash <158308964+MeAkash77@users.noreply.github.com>
2026-03-01 23:56:22 +0530

    Update Mnist1.py

--- DigitSense/Mnist1.py
+++ DigitSense/Mnist1.py
@@ -22,10 +22,10 @@ st.set_page_config(page_title="MNIST Digit Recognition", page_icon="🧮", layou
 # lottie_animation = load_lottie_url(lottie_url)
 
 def load_lottie_file(filepath: str):
-    base_dir = os.path.dirname(__file__)
+     base_dir = os.path.dirname(__file__)
     filepath = os.path.join(base_dir, filename)
     
-    with open(filepath, "r") as f:
+        with open(filepath, "r", encoding="utf-8") as f:
         return json.load(f)
 
 # Path to the Lottie file
